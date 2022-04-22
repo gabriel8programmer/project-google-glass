e1bd1438e9afe41a1d332076aa0672eca6459f89
gabriel8programmer <gabriel8github@gmail.com>
2022-04-21 22:59:46 -0300

    site completo

---
+++ _javascript/funcoes.js
@@ -0,0 +1,12 @@
+
+//função para mudar o icone na pagina principal
+function MudarIcone(icone){
+    document.getElementById("icone").src = `_imagens/${icone}`;
+}
+
+/*funcão para calcular o total em vendas*/
+function calcularTotal(preco){
+    const qtd = parseInt(document.getElementById("qtd").value);
+    const tot = preco * qtd;
+    document.getElementById("precototal").value = tot;
+}
