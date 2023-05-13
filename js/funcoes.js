d9a304db00b0b87221a64d0a9ae25fa2dfa8c9c7
gabriel8programmer <gabriel8github@gmail.com>
2023-05-12 21:54:15 -0300

    change names in project

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,11 +1,11 @@
 
 //função para mudar o icone na pagina principal
-function MudarIcone(icone){
-    document.getElementById("icone").src = `_imagens/${icone}`;
+function MudarIcone(icone) {
+    document.getElementById("icone").src = `images/${icone}`;
 }
 
 /*funcão para calcular o total em vendas*/
-function calcularTotal(preco){
+function calcularTotal(preco) {
     const qtd = parseInt(document.getElementById("qtd").value);
     const tot = preco * qtd;
     document.getElementById("precototal").value = tot;
